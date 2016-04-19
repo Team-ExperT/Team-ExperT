586157caa3ff5d03d96887531203efdb94cd3cdb
spondbob <spondbob@eamca.com>
2016-04-19 11:47:36 +1000

    add documentations

--- js/freelancer.js
+++ js/freelancer.js
@@ -36,6 +36,7 @@ $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
     $('.navbar-toggle:visible').click();
 });
 
+// Map handler
 $(function() {
   var stage = new createjs.Stage("viewport");
 
@@ -46,6 +47,8 @@ $(function() {
     var bitmap = new createjs.Bitmap(base_image);
     stage.enableMouseOver(10);
     stage.addChild(bitmap);
+
+    // Clear map when clicked
     bitmap.addEventListener("click", function(event) {
        $(".myObj").popover('hide');
     });
@@ -58,12 +61,20 @@ $(function() {
       $.getJSON("http://vicsurv.cloudapp.net:5780/api/get_daily_levels", function( data ) {
         $.each(data, function(i, item) {
           bitmap_marker[i] = new createjs.Bitmap(marker);
-          bitmap_marker[i].alpha = 0.5;
+
+          // Popover position
           bitmap_marker[i].x = item.x - 25;
           bitmap_marker[i].y = item.y - 25;
+
+          // Animating popover on hover
+          bitmap_marker[i].alpha = 0.5;
           bitmap_marker[i].on("mouseover", handleInteraction);
           bitmap_marker[i].on("mouseout", handleInteraction);
+
+          // On click listener or popover
           bitmap_marker[i].addEventListener("click", function(event) {
+
+            // Set popover content
             ni = (item.ni_p50/13.4*100).toFixed(1);
             ox = (item.ox_p50/11*100).toFixed(1);
             ph = (item.ph_p50/2.4*100).toFixed(1);
@@ -71,21 +82,27 @@ $(function() {
             content = "<p>" + item.region + "</p>Nitrogen: " + ni + "%<br/>Oxygen: " + ox + "%<br/>Phosphorus: " + ph + "%<br/>Total suspended solids: " + ts + "%"
             $(".myObj").attr('data-content', content);
             $(".myObj").attr('data-original-title', item.area);
+
+            // Build the popover
             $(".myObj").css({'position':'absolute','top':item.y,'left':item.x + 15}).popover({
                 html : true,
                 trigger: 'click',
                 placement:'top'
             }).popover('show');
           });
+
+          // Add the marker to the stage
           stage.addChild(bitmap_marker[i]);
         });
 
+        // Draw the stage
         stage.update();
         createjs.Ticker.addEventListener("tick", stage);
       });
     };
   };
 
+  // Hover handler
   function handleInteraction(event) {
     event.target.alpha = (event.type == "mouseover") ? 1 : 0.5; 
   }
