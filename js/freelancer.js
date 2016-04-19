d3de3190cdbb82405b36cb9c4023ff68449d2870
spondbob <spondbob@eamca.com>
2016-04-19 11:43:55 +1000

    add hover animation

--- js/freelancer.js
+++ js/freelancer.js
@@ -44,6 +44,7 @@ $(function() {
 
   base_image.onload = function(){
     var bitmap = new createjs.Bitmap(base_image);
+    stage.enableMouseOver(10);
     stage.addChild(bitmap);
     bitmap.addEventListener("click", function(event) {
        $(".myObj").popover('hide');
@@ -57,15 +58,18 @@ $(function() {
       $.getJSON("http://vicsurv.cloudapp.net:5780/api/get_daily_levels", function( data ) {
         $.each(data, function(i, item) {
           bitmap_marker[i] = new createjs.Bitmap(marker);
+          bitmap_marker[i].alpha = 0.5;
           bitmap_marker[i].x = item.x - 25;
           bitmap_marker[i].y = item.y - 25;
+          bitmap_marker[i].on("mouseover", handleInteraction);
+          bitmap_marker[i].on("mouseout", handleInteraction);
           bitmap_marker[i].addEventListener("click", function(event) {
             ni = (item.ni_p50/13.4*100).toFixed(1);
             ox = (item.ox_p50/11*100).toFixed(1);
             ph = (item.ph_p50/2.4*100).toFixed(1);
             ts = (item.ts_p50/47*100).toFixed(1);
             content = "<p>" + item.region + "</p>Nitrogen: " + ni + "%<br/>Oxygen: " + ox + "%<br/>Phosphorus: " + ph + "%<br/>Total suspended solids: " + ts + "%"
-            $(".myObj").attr('data-content', content)
+            $(".myObj").attr('data-content', content);
             $(".myObj").attr('data-original-title', item.area);
             $(".myObj").css({'position':'absolute','top':item.y,'left':item.x + 15}).popover({
                 html : true,
@@ -77,7 +81,12 @@ $(function() {
         });
 
         stage.update();
+        createjs.Ticker.addEventListener("tick", stage);
       });
     };
   };
+
+  function handleInteraction(event) {
+    event.target.alpha = (event.type == "mouseover") ? 1 : 0.5; 
+  }
 });
